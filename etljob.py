34882fc2bc8f07f1df54418241a900d8eee0e9ad
astankie112358 <astankie@wp.pl>
2025-11-27 22:28:24 +0000

    Change lineage for all tables

--- etljob.py
+++ etljob.py
@@ -9,20 +9,35 @@ bronze_schema = "bronze"
 silver_schema = "silver"
 gold_schema = "gold"
 tables_df = dbutils.fs.ls("/Volumes/workspace/source/lake/")
-tables = [row.name for row in tables_df]
-for table in tables:
-    csv_name=table
-    table_name=table.replace(".csv","")
-    @dlt.create_table(name=f"{catalog}.{bronze_schema}.{table_name}")
-    def create_table(table_name=table):
-        df=spark.read.format("csv").option("header", "true").load(f"/Volumes/workspace/source/lake/{csv_name}")
-        return df
 
-for table in tables:
-    table.replace(".csv","")
-    table=table.replace(".csv","")  
-    @dlt.create_table(name=f"{catalog}.{silver_schema}.{table}")
-    def create_table(table_name=table):
-        df=spark.sql(f"SELECT * FROM {bronze_schema}.b_{table}")
-        return df
+@dlt.table(
+    name=f"{catalog}.{bronze_schema}.ETLFILMY",
+)
+def bronze_ETLFILMY():
+    df = spark.read.format("csv").option("header", "true").load(
+        f"/Volumes/workspace/source/lake/ETLFILMY.csv"
+    )
+    return df
+@dlt.table(name=f"{catalog}.{silver_schema}.ETLFILMY")
+def silver_ETLFILMY():
+    df = dlt.read(f"{catalog}.{bronze_schema}.ETLFILMY")
+    df = add_loaded_time(df)
+    return df
+
+# tables = [row.name for row in tables_df]
+# for table in tables:
+#     csv_name=table
+#     table_name=table.replace(".csv","")
+#     @dlt.create_table(name=f"{catalog}.{bronze_schema}.{table_name}")
+#     def create_table(table_name=table):
+#         df=spark.read.format("csv").option("header", "true").load(f"/Volumes/workspace/source/lake/{csv_name}")
+#         return df
+
+# for table in tables:
+#     table.replace(".csv","")
+#     table=table.replace(".csv","")  
+#     @dlt.create_table(name=f"{catalog}.{silver_schema}.{table}")
+#     def create_table(table_name=table):
+#         df=spark.sql(f"SELECT * FROM {bronze_schema}.b_{table}")
+#         return df
 
