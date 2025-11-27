c882fdaba00ab3cea64b149b00e525f2f3c2efe0
astankie112358 <astankie@wp.pl>
2025-11-27 21:35:22 +0000

    Working with schemas

--- etljob.py
+++ etljob.py
@@ -3,29 +3,26 @@ from pyspark.sql.functions import current_timestamp
 from src.transformations.add_loaded_time import add_loaded_time
 import dlt
 
-def read_table(schema, table):
-    return SparkSession.getActiveSession().table(f"{schema}.{table}")
-
-def write_table(df, schema, table, mode="overwrite"):
-    (
-        df.write
-          .format("delta")
-          .mode(mode)
-          .saveAsTable(f"{schema}.{table}")
-    )
-
+catalog="workspace"
 source_schema = "source"
+bronze_schema = "bronze"
+silver_schema = "silver"
+gold_schema = "gold"
 tables_df = dbutils.fs.ls("/Volumes/workspace/source/lake/")
 tables = [row.name for row in tables_df]
-
-print("Starting ETL pipeline...")
-
 for table in tables:
-    print(f"\n---- Processing table: {table} ----")
-    
-    @dlt.table(name=f"stg_{table}")
+    csv_name=table
+    table_name=table.replace(".csv","")
+    @dlt.create_table(name=f"{catalog}.{bronze_schema}.{table_name}")
     def create_table(table_name=table):
-        df=spark.read.format("csv").option("header", "true").load(f"/Volumes/workspace/source/lake/{table}")
+        df=spark.read.format("csv").option("header", "true").load(f"/Volumes/workspace/source/lake/{csv_name}")
         return df
 
-print("\nETL pipeline completed successfully.")
+for table in tables:
+    table.replace(".csv","")
+    table=table.replace(".csv","")  
+    @dlt.create_table(name=f"{catalog}.{silver_schema}.{table}")
+    def create_table(table_name=table):
+        df=spark.sql(f"SELECT * FROM {bronze_schema}.b_{table}")
+        return df
+
