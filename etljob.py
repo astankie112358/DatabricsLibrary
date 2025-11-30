db23b78d9ad92ef276c842294134648a16b79e78
astankie112358 <astankie@wp.pl>
2025-11-30 20:47:25 +0000

    Working version denormalized with aggregations

--- etljob.py
+++ etljob.py
@@ -2,7 +2,7 @@ from pyspark.sql.functions import current_timestamp
 from src.transformations.add_loaded_time import add_loaded_time
 from src.transformations.rename_columns import rename_columns
 from src.transformations.rename_tables import get_new_table_name
-from src.transformations.create_joins import return_joins_for_table
+from src.transformations.create_joins import find_join_map
 import dlt
 
 catalog="workspace"
@@ -50,7 +50,7 @@ def silver_table_stream(table_name):
     new_name = get_new_table_name(table_name)
     @dlt.table(name=f"{catalog}.{silver_schema}.{new_name}")
     def _table():
-        df = spark.readStream.table(f"{catalog}.{bronze_schema}.{table_name}")
+        df = spark.read.table(f"{catalog}.{bronze_schema}.{table_name}")
         df = add_loaded_time(df)
         df = rename_columns(df, table_name)
         return df
@@ -66,31 +66,22 @@ silver_table_stream("ETLRODZAJE")
 silver_table_stream("ETLWYPO")
 silver_table_stream("ETLWYPO_NOS")
 
-def gold_fact_table(table_name):
-    new_name = "f_orders"
-    @dlt.table(name=f"{catalog}.{gold_schema}.{new_name}")
+def gold_table(table_name, new_table_name, join_tables=None, drop_columns=None):
+    @dlt.table(name=f"{catalog}.{gold_schema}.{new_table_name}")
     def _table():
-        df = spark.readStream.table(f"{catalog}.{silver_schema}.{table_name}")
-        df2 = spark.readStream.table(f"{catalog}.{silver_schema}.rental_items")
-        df = df.join(
-            df2,
-            df2.rental_id == df.rental_id
-        )
-        df = df.drop(df2.rental_id, df2.loaded_time)
-        return df
-def gold_dim_table(table_name):
-    new_name = "d_movies"
-    @dlt.table(name=f"{catalog}.{gold_schema}.{new_name}")
-    def _table():
-        df = spark.readStream.table(f"{catalog}.{silver_schema}.{table_name}")
-        df2 = spark.readStream.table(f"{catalog}.{silver_schema}.movie_genre")
-        df3 = spark.readStream.table(f"{catalog}.{silver_schema}.movie_country")
-        df4 = spark.readStream.table(f"{catalog}.{silver_schema}.genres")
-        df5 =
-        df = df.join(
-            df2
-        return df
-
-gold_fact_table("rentals")
+        df_main = spark.read.table(f"{catalog}.{silver_schema}.{table_name}")
+        joins={}
+        if join_tables is not None:
+            joins.update({table_name: spark.read.table(f"{catalog}.{silver_schema}.{table_name}") for table_name in join_tables})
+            df_main=find_join_map(df_main, table_name, joins, drop_columns=None)
+        return df_main
         
+table_name="copies"
+join_tables=["directors", "movie_country", "movie_genre","genres","countries","movies"]
+drop_columns=["movie_id", "director_id"]     
+
+gold_table(table_name, "d_movies", join_tables, drop_columns)
+gold_table("clients", "d_customers")
+gold_table("rental_items", "f_orders",["rentals"])
+
 
--- src/transformations/create_joins.py
+++ src/transformations/create_joins.py
@@ -1,52 +1,53 @@
 import src.errors.custom_errors as e
+import pyspark.sql.functions as F
 
 join_config = {
-    "Movies": {
-        "Directors": {"on": "director_id", "how": "left", "prefix": "dir_"},
-        "Movie_Country": {"on": "movie_id", "how": "left", "prefix": "mc_"},
-        "Movie_Genre": {"on": "movie_id", "how": "left", "prefix": "mg_"},
-        "Copies": {"on": "movie_id", "how": "left", "prefix": "copy_"}
+    "movies": {
+        "directors": {"on": "director_id", "how": "left", "prefix": "dir_"},
+        "movie_country": {"on": "movie_id", "how": "left", "prefix": "mc_"},
+        "movie_genre": {"on": "movie_id", "how": "left", "prefix": "mg_"},
+        "copies": {"on": "movie_id", "how": "left", "prefix": "copy_"}
     },
 
-    "Directors": {
-        "Movies": {"on": "director_id", "how": "left", "prefix": "movie_"}
+    "directors": {
+        "movies": {"on": "director_id", "how": "left", "prefix": "movie_"}
     },
 
-    "Countries": {
-        "Movie_Country": {"on": "country_id", "how": "left", "prefix": "mc_"}
+    "countries": {
+        "movie_country": {"on": "country_id", "how": "left", "prefix": "mc_"}
     },
 
-    "Movie_Country": {
-        "Movies": {"on": "movie_id", "how": "left", "prefix": "movie_"},
-        "Countries": {"on": "country_id", "how": "left", "prefix": "country_"}
+    "movie_country": {
+        "movies": {"on": "movie_id", "how": "left", "prefix": "movie_"},
+        "countries": {"on": "country_id", "how": "left", "prefix": "country_"}
     },
 
-    "Genres": {
-        "Movie_Genre": {"on": "genre_id", "how": "left", "prefix": "mg_"}
+    "genres": {
+        "movie_genre": {"on": "genre_id", "how": "left", "prefix": "mg_"}
     },
 
-    "Movie_Genre": {
-        "Movies": {"on": "movie_id", "how": "left", "prefix": "movie_"},
-        "Genres": {"on": "genre_id", "how": "left", "prefix": "genre_"}
+    "movie_genre": {
+        "movies": {"on": "movie_id", "how": "left", "prefix": "movie_"},
+        "genres": {"on": "genre_id", "how": "left", "prefix": "genre_"}
     },
 
-    "Clients": {
-        "Rentals": {"on": "client_id", "how": "left", "prefix": "rent_"}
+    "clients": {
+        "rentals": {"on": "client_id", "how": "left", "prefix": "rent_"}
     },
 
-    "Rentals": {
-        "Clients": {"on": "client_id", "how": "left", "prefix": "client_"},
-        "Rental_Items": {"on": "rental_id", "how": "left", "prefix": "item_"}
+    "rentals": {
+        "clients": {"on": "client_id", "how": "left", "prefix": "client_"},
+        "rental_items": {"on": "rental_id", "how": "left", "prefix": "item_"}
     },
 
-    "Rental_Items": {
-        "Rentals": {"on": "rental_id", "how": "inner", "prefix": "rent_"},
-        "Copies": {"on": "carrier_id", "how": "inner", "prefix": "copy_"}
+    "rental_items": {
+        "rentals": {"on": "rental_id", "how": "inner", "prefix": "rent_"},
+        "copies": {"on": "carrier_id", "how": "inner", "prefix": "copy_"}
     },
 
-    "Copies": {
-        "Movies": {"on": "movie_id", "how": "left", "prefix": "movie_"},
-        "Rental_Items": {"on": "carrier_id", "how": "left", "prefix": "item_"}
+    "copies": {
+        "movies": {"on": "movie_id", "how": "left", "prefix": "movie_"},
+        "rental_items": {"on": "carrier_id", "how": "left", "prefix": "item_"}
     }
 }
 
@@ -54,13 +55,13 @@ def check_if_tables_join(table1, table2):
     return table2 in join_config[table1].keys()
 
 def prefix_columns(df, prefix):
-    return df.select([df[col].alias(prefix + col) for col in df.columns])
+    return df.select([df[col].alias(f"{prefix}_{col}") for col in df.columns])
 
 def joined_tables(source_name, target_name, df1, df2, old_prefix=None, prefix=None):
     join_key = join_config[source_name][target_name]['on']
     df2 = prefix_columns(df2, prefix)
-    left_key  = (old_prefix + join_key) if old_prefix else join_key
-    right_key = prefix + join_key
+    left_key  = f"{old_prefix}_{join_key}" if old_prefix else join_key
+    right_key = f"{prefix}_{join_key}"
     how=join_config[source_name][target_name]['how']
     return df1.join(
         df2,
@@ -68,36 +69,59 @@ def joined_tables(source_name, target_name, df1, df2, old_prefix=None, prefix=No
         how
     )
 
-def find_join_map(table_name, joins):
-    tables_to_read = [table_name] + joins
-    dfs = {
-        t: spark.read.table(f"workspace.silver.{t}")
-        for t in tables_to_read
-    }
-    target_df = spark.read.table(f"workspace.silver.{table_name}")
-    left_to_join = joins.copy()
+def aggregate_tables(df, matched, main_table):
+    main_cols = [col for col in df.columns if not any(col.startswith(prefix + "_") for prefix in matched.values())]
+    added_cols = [col for col in df.columns if any(col.startswith(prefix + "_") for prefix in matched.values())]
+    return df.groupBy(main_cols).agg(*[F.collect_list(col).cast("string").alias(col) for col in added_cols])
+
+def remove_columns(df, columns, matched):
+    cols_to_remove = set()
+    for table, prefix in matched.items():
+        for col in columns:
+            prefixed_col = f"{prefix}_{col}"
+            if prefixed_col in df.columns:
+                cols_to_remove.add(prefixed_col)
+    return df.select([col for col in df.columns if col not in cols_to_remove])
+
+def find_join_map(df, table_name, joins, drop_columns=None):
+    tables_to_read = [table_name] + list(joins.keys())
+    dfs = {table_name: df}
+    dfs.update(joins)
+    target_df = df
+    left_to_join = list(joins.keys())
     matched = {}
-    for other_table in joins:
+    for other_table in left_to_join[:]:
         if check_if_tables_join(table_name, other_table):
-            prefix=other_table
-            matched.update({other_table:prefix})
+            prefix = other_table
+            matched.update({other_table: prefix})
             left_to_join.remove(other_table)
-            other_df = dfs[other_table]    
-            target_df=joined_tables(table_name, other_table, target_df, other_df, prefix=prefix)
-    while len(left_to_join)>0:
-        operation_made=False
+            other_df = dfs[other_table]
+            target_df = joined_tables(table_name, other_table, target_df, other_df, prefix=prefix)
+    while len(left_to_join) > 0:
+        operation_made = False
         for other_table in left_to_join[:]:
             for matched_table in list(matched.keys()):
                 if check_if_tables_join(other_table, matched_table):
                     other_df = dfs[other_table]
-                    prefix=matched[matched_table]
-                    other_prefix=f"{other_table}_"
-                    matched.update({other_table:other_prefix})
+                    prefix = matched[matched_table]
+                    other_prefix = f"{other_table}"
+                    matched.update({other_table: other_prefix})
                     left_to_join.remove(other_table)
-                    operation_made=True
-                    target_df=joined_tables(matched_table, other_table, target_df, other_df, prefix=other_prefix, old_prefix=prefix)
+                    operation_made = True
+                    target_df = joined_tables(matched_table, other_table, target_df, other_df, prefix=other_prefix, old_prefix=prefix)
         if not operation_made:
             raise e.CantMapSelectedTables(left_to_join)
+    target_df = aggregate_tables(target_df, matched, table_name)
+    if drop_columns:
+        target_df = remove_columns(target_df, drop_columns, matched)
     return target_df
 
-display(find_join_map("Movies", ["Movie_Genre"]))
+# catalog="workspace"
+# silver_schema="silver"
+# table_name="movies"
+# join_tables=["directors", "movie_country", "movie_genre","genres","countries"]
+# drop_columns=["movie_id", "director_id"]
+
+# df = spark.read.table(f"{catalog}.{silver_schema}.{table_name}")
+# df=find_join_map(df,table_name, join_tables, drop_columns)
+# display(df)
